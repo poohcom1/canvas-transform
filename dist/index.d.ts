284dfa657357cdefa21b0a31bd96925472e0845f
poohcom1 <pooh677@gmail.com>
2022-07-08 08:45:51 +0900

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+interface TransformCanvasRenderingContext2D extends CanvasRenderingContext2D {
+    getTransform(): SVGMatrix;
+    transformedPoint(x: number, y: number): SVGPoint;
+}
+export declare function trackTransforms(ctx: CanvasRenderingContext2D | any): TransformCanvasRenderingContext2D;
+export declare function getTransformContext(canvas: HTMLCanvasElement): (callback: (ctx: TransformCanvasRenderingContext2D) => void) => void;
+export {};
